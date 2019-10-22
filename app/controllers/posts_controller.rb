23360ba46a66c3f2740dd880a0c5abce78e09a39
Will Hodgson <will.hodgson@current-rms.com>
2019-10-22 10:06:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.requite(post))
+	  @post.update(params.requite(:post))
 	  redirect_to post_path(@post)
 	end
 
